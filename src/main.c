2f35ce7184b4e3d969ac6da3edd7c2bc665f1d60
Edson <edson.reyes@usach.cl>
2020-07-14 20:39:15 -0400

    Code almost ready

--- src/main.c
+++ src/main.c
@@ -4,9 +4,7 @@
 #include <unistd.h>
 #include <getopt.h>
 
-#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
 #include "funciones.h"
@@ -19,7 +17,7 @@ int main (int argc, char **argv){
     char* filtro = NULL;
     recibirDatos(argc, argv, &cantImg, &umbral, &porcentaje, &filtro, &bandera);
     if(bandera == 1){
-        printf("|    image    |    nearly black    |\n|-------------|--------------------|\n");
+        printf("\n------------------------------------\n|    image    |    nearly black    |\n|-------------|--------------------|\n");
     }
     
     for(int i = 1; i <= cantImg; i++){
@@ -30,11 +28,9 @@ int main (int argc, char **argv){
         unsigned char *imgBN = convertirBN(img, &ancho, &alto, &canales);
         unsigned char *imgFiltro = aplicarFiltro(imgBN, &ancho, &alto, filtro);
         unsigned char *imgBin = binarizar(imgFiltro, &ancho, &alto, umbral);
-        escribirImgBN(imgBin, ancho, alto, imgBin);
-
         char* salida = malloc(32);
         sprintf(salida, "imagenes_salida/imagen_%d.jpg", i);
-        stbi_write_jpg(salida, ancho, alto, 1, imgBin, 100);
+        escribirImgBN(salida, ancho, alto, imgBin);
         int clasificacion = isNearlyBlack(imgBin, &ancho, &alto, porcentaje);
         if(bandera == 1 && clasificacion == 1){
             printf("|  imagen_%d   |         yes        |\n", i);
@@ -42,5 +38,9 @@ int main (int argc, char **argv){
         else if(bandera == 1 && clasificacion == 0){
             printf("|  imagen_%d   |         no         |\n", i);
         }
+    }
+
+    if(bandera == 1){
+        printf("------------------------------------\n\n");
     }
 }
